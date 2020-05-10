b21076967b182db6e3afbfcdd722946d659e2494
Andre Santos Barros da Silva <andrezio@live.com>
2020-05-10 09:47:04 -0300

    right correct

--- game/jogo.gd
+++ game/jogo.gd
@@ -10,6 +10,7 @@ var plus := 82
 
 onready var default = preload("res://cenas/default.tscn")
 onready var twopiece = preload("res://pieces/2piece.tscn")
+onready var fourpiece = preload("res://pieces/4piece.tscn")
 
 var board = [[null,null,null,null],[null,null,null,null],[null,null,null,null],[null,null,null,null]]	
 var pos_matrix =  [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
@@ -23,77 +24,97 @@ func update_piece(piece, i, j):
 	n_piece.position = grid_to_pixel(Vector2(i,j))
 	board[i][j] = n_piece
 
-func update_one_piece(piece, i, j, plus):
-	piece.move(grid_to_pixel(Vector2(i+plus, j)))
+func update_one_piece(piece, i, j, plus_x, plus_y=0):
+	piece.move(grid_to_pixel(Vector2(i+plus_x, j+plus_y)))
 	board[i][j] =null
-	board[i+plus][j] =piece
+	board[i+plus_x][j+plus_y] =piece
 
-func update_two_pieces(piece_one, piece_two, i, j , plus):
+func update_two_pieces(piece_one, piece_two, i, j , plus_x, plus_y=0):
 	# move a peca
-	piece_one.move(grid_to_pixel(Vector2(i+plus, j)))
+	var vector = Vector2(i+plus_x, j+plus_y)
+	piece_one.move(grid_to_pixel(vector))
 
 	# cria peca de soma
 	var new_piece = piece_one.next_piece.instance()
 	add_child(new_piece)
-	new_piece.position = grid_to_pixel(Vector2(i+plus,j))
+	new_piece.position = grid_to_pixel(vector)
 
 	piece_one.remove()
 	piece_two.remove()
-	board[i+plus][j] = null
+
+	board[i+plus_x][j+plus_y] = null
 	board[i][j] = null
-	board[i+plus][j] = new_piece
+	board[i+plus_x][j+plus_y] = new_piece
+
+
+func thirth_piece(temp, i,j):
+	var value = temp.value
+	if board[i+1][j]==null && board[i+2][j]==null && board[i+3][j]==null:
+		var plus = 3
+		update_one_piece(temp, i, j, plus)
+		return true
+	if board[i+1][j]==null && board[i+2][j]==null && board[i+3][j].value==value:
+		var plus = 3
+		update_two_pieces(temp, board[i+plus][j],i,j,plus)
+		return true
+	if board[i+1][j]==null && board[i+2][j]==null && board[i+3][j].value!=value:
+		var plus = 2
+		update_one_piece(temp, i, j, plus)
+		return true
+	return false
+
+func second_piece(temp, i, j):
+	var value = temp.value
+	if board[i+1][j]==null && board[i+2][j]==null:
+		var plus = 2
+		update_one_piece(temp, i, j, plus)
+		return true
+	if board[i+1][j]==null && board[i+2][j].value==value:
+		var plus = 2
+		update_two_pieces(temp, board[i+plus][j],i,j,plus)
+		return true
+	if board[i+1][j]==null && board[i+2][j].value!=value:
+		var plus = 1
+		update_one_piece(temp, i, j, plus)
+		return true
+	return false
+
+func first_piece(temp, i,j):
+	var value = temp.value
+	if board[i+1][j]==null :
+		var plus = 1
+		update_one_piece(temp, i, j ,plus)
+		return true
+	if board[i+1][j].value==value :
+		var plus = 1
+		update_two_pieces(temp, board[i+plus][j], i, j ,plus)
+		return true
+	return false
 
 func move_piece(piece, i,j ,direction):
 
 	var temp = board[i][j]
-	var value = board[i][j].value
+	var value = temp.value
 
 	match direction:
 		Vector2.RIGHT:
 			if i ==0:
-				if board[i+1][j]==null && board[i+2][j]==null && board[i+3][j]==null:
-					var plus = 3
-					update_one_piece(temp, i, j, plus)
+				if thirth_piece(temp, i, j): 
 					return
-				if board[i+1][j]==null && board[i+2][j]==null && board[i+3][j].value==value:
-					var plus = 3
-					update_two_pieces(temp, board[i+plus][j],i,j,plus)
+				if second_piece(temp, i, j):
 					return
-				if board[i+1][j]==null && board[i+2][j]==null && board[i+3][j].value!=value:
-					var plus = 2
-					update_one_piece(temp, i, j, plus)
-					return
-				if board[i+1][j]==null && board[i+2][j].value==value:
-					var plus = 2
-					update_two_pieces(temp, board[i+plus][j],i,j,plus)
-					return
-				if board[i+1][j]==null && board[i+2][j].value!=value:
-					var plus = 1
-					update_one_piece(temp, i, j, plus)
+				if first_piece(temp, i, j):
 					return
 			if i ==1:
-				if board[i+1][j]==null && board[i+2][j]==null:
-					var plus = 2
-					update_one_piece(temp, i, j, plus)
+				if second_piece(temp, i, j):
 					return
-				if board[i+1][j]==null && board[i+2][j].value==value:
-					var plus = 2
-					update_two_pieces(temp, board[i+plus][j],i,j,plus)
-					return
-				if board[i+1][j]==null && board[i+2][j].value!=value:
-					var plus = 1
-					update_one_piece(temp, i, j, plus)
+				if first_piece(temp, i, j):
 					return
 			if i ==2:
-				if board[i+1][j]==null :
-					var plus = 1
-					update_one_piece(temp, i, j ,plus)
+				if first_piece(temp, i, j):
 					return
-				if board[i+1][j].value==value :
-					var plus = 1
-					update_two_pieces(temp, board[i+plus][j], i, j ,plus)
-					return
-				
+		Vector2.LEFT:
+			pass
 
 func move_right():
 	var temp_new_position
@@ -102,9 +123,16 @@ func move_right():
 		for i in [2,1,0]:
 			if board[i][j] != null:
 				move_piece(piece, i,j, Vector2.RIGHT)
-				printar()
+				#printar()
+
 func move_left():
-	pass
+	var temp_new_position
+	var piece = board[0][0]
+	for j in height:
+		for i in [0,1,2]:
+			if board[i][j] != null:
+				move_piece(piece, i,j, Vector2.LEFT)
+				#printar()
 
 func move_up():
 	pass
@@ -130,7 +158,6 @@ func made_bg():
 			add_child(p)
 			pos_matrix[i][j] = grid_to_pixel(Vector2(i,j)) 
 
-
 func generate_piece():
 	randomize()
 	var pos_x = randi()%3 + 1
@@ -146,10 +173,18 @@ func generate_piece():
 		return true
 	return false
 
-
-func one_piece_generate(pos_x, pos_y):
+func one_piece_generate_2(pos_x, pos_y):
 	if board[pos_x][pos_y]==null:
 		var temp = twopiece.instance()
+		temp.position = grid_to_pixel(Vector2(pos_x, pos_y))
+		add_child(temp)
+		board[pos_x][pos_y] = temp
+		return true
+	return false
+
+func one_piece_generate_4(pos_x, pos_y):
+	if board[pos_x][pos_y]==null:
+		var temp = fourpiece.instance()
 		temp.position = grid_to_pixel(Vector2(pos_x, pos_y))
 		add_child(temp)
 		board[pos_x][pos_y] = temp
@@ -164,10 +199,10 @@ func printar():
 func _ready():
 	made_bg()
 	#generate_piece()
-	one_piece_generate(0,0)
-	one_piece_generate(1,0)
-	one_piece_generate(2,0)
-	one_piece_generate(3,0)
+	one_piece_generate_2(0,0)
+	#one_piece_generate_2(1,0)
+	#one_piece_generate_2(2,0)
+	#one_piece_generate_2(3,0)
 	#printar()
 	pass # Replace with function body.
 
